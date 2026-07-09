f238342b44b8bbccc5b15d9f20f6f44f15f8c48e
Kilian Fogarty <kilian.fogarty@icloud.com>
2026-07-09 15:04:43 -0800

    chore: Spring Initializr necessary packages added for backend.

---
+++ FixCarolina_backend/src/main/java/com/kilianfogarty/FixCarolina/FixCarolinaApplication.java
@@ -0,0 +1,13 @@
+package com.kilianfogarty.FixCarolina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixCarolinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixCarolinaApplication.class, args);
+	}
+
+}
---
+++ FixCarolina_backend/src/test/java/com/kilianfogarty/FixCarolina/FixCarolinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kilianfogarty.FixCarolina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixCarolinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
