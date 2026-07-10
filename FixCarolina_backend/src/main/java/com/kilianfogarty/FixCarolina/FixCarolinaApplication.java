d116d9454280628946dbbe782318d64ca083377f
Kilian Fogarty <kilian.fogarty@icloud.com>
2026-07-09 21:22:07 -0800

    fix: changes main application logic to public

--- FixCarolina_backend/src/main/java/com/kilianfogarty/FixCarolina/FixCarolinaApplication.java
+++ FixCarolina_backend/src/main/java/com/kilianfogarty/FixCarolina/FixCarolinaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FixCarolinaApplication {
 
-  static void main(String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(FixCarolinaApplication.class, args);
   }
 }
