afb90a9aa117d7dce922f3c1a974af6e454689c5
Kilian Fogarty <kilian.fogarty@icloud.com>
2026-07-09 18:26:27 -0800

    chore: Created simple User and Post classes.

---
+++ FixCarolina_backend/src/main/java/com/kilianfogarty/FixCarolina/Post.java
@@ -0,0 +1,53 @@
+package com.kilianfogarty.FixCarolina;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @JoinColumn(name = "user_id", nullable = false, updatable = false)
+  private User user;
+
+  @Column(nullable = false)
+  private String imagePath;
+
+  @Column(nullable = false)
+  private String description;
+
+  @Column(nullable = false)
+  private String locationText;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Status status;
+
+  @Column(nullable = false, updatable = false)
+  private Instant creationTime;
+
+  public Post(String imagePath, String description, String locationText) {
+    this.user = user;
+    this.imagePath = imagePath;
+    this.description = description;
+    this.locationText = locationText;
+  }
+
+  @PrePersist
+  private void onCreation() {
+    this.creationTime = Instant.now();
+    if (this.status == null) {
+      this.status = Status.PENDING;
+    }
+  }
+}
---
+++ FixCarolina_backend/src/main/java/com/kilianfogarty/FixCarolina/User.java
@@ -0,0 +1,38 @@
+package com.kilianfogarty.FixCarolina;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String username;
+
+  @Column(nullable = false)
+  private String hashedPassword;
+
+  @Column(nullable = false, updatable = false)
+  private Instant creationTime;
+
+  public User(String username, String hashedPassword) {
+    this.username = username;
+    this.hashedPassword = hashedPassword;
+  }
+
+  @PrePersist
+  private void onCreation() {
+    this.creationTime = Instant.now();
+  }
+}
